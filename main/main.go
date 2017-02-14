664ca72fa92c5a248ed705e17f2bd804db19b873
Adriabs <adrianskibelid@hotmail.com>
2017-02-14 13:48:51 +0100

    added

---
+++ main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	//"./Interface/driver"
+	"./Network"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello Github!")
+
+	//driver.Elev_init()
+
+	//driver.Elev_set_motor_direction(-1)
+	Netmain.Netmain()
+}
