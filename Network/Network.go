73918a258639eee1adba01d1d810c90aefbe74ee
petermst <kji>
2017-03-01 11:58:18 +0100

    YoYo

--- Network/Network.go
+++ Network/Network.go
@@ -79,3 +79,26 @@ func Network() {
 		}
 	}
 }
+
+func Initialize_network() {
+	/*
+		//Initializing network
+		peer_rx_updates := make(chan peers.PeerUpdate)
+		peer_tx_enable := make(chan bool)
+		go peers.Transmitter(28455, id, peer_tx_enable)
+		go peers.Receiver(28455, peer_rx_updates)
+
+		net_tx_requests := make(chan Request)
+		net_rx_requests := make(chan Request)
+		net_tx_states := make(chan ElevatorUpdate)
+		net_rx_states := make(chan ElevatorUpdate)
+		net_tx_cabRequestRestore := make(chan CabRequestRestore)
+		net_rx_cabRequestRestore := make(chan CabRequestRestore)
+		go bcast.Transmitter(28456, net_tx_requests, net_tx_states, net_tx_cabRequestRestore)
+		go bcast.Receiver(28456, net_rx_requests, net_rx_states, net_rx_cabRequestRestore)
+	*/
+	StopButtonPressed := make(chan int)
+	Peer_rx_stop_button_pressed := make(chan StopButtonPressed)
+	go bcast.Transmitter(28456, Peer_rx_stop_button_pressed)
+	go bcast.Receiver(28456, Peer_rx_stop_button_pressed)
+}
