44b6d16f3e47ff0c118b0d6ea3eebfba415b6880
Astrid Smith Løvås <astridsmithlovas@dhcp-10-22-74-191.wlan.ntnu.no>
2017-03-10 14:59:40 +0100

    heitroololo

--- V1/statemachine.go
+++ V1/statemachine.go
@@ -9,11 +9,11 @@ import{
 
 //Defining variables (using variables from config (driver))
 var state def.State
-var dir def.MotorDirection
-var prevDir def.MotorDirection
+var direction def.MotorDirection
+var prevDirection def.MotorDirection
 var lastFloor int
 
-func initialize() {
+func initialize(arrivedAtFloor, orderCompleted chan int, newOrder, doorTimeout, doorTimerReset, updateStatus, startOrderTimer, endOrderTimer, orderTimeout chan bool){
 	//Make channels for door and order timer functions
 	//Order timer:
 	startOrderTimer := make(chan, bool)
@@ -25,8 +25,8 @@ func initialize() {
 
 	//Choose which "positions" the variables should be in
 	state = def.Idle
-	dir = def.MD_stop
-	prevDir = def.MD_down
+	direction = def.MotorD_stop
+	prevDirection = def.MotorD_down
 	lastFloor = floor
 
 
@@ -34,7 +34,7 @@ func initialize() {
 	//Run go routines. (Handle door timer and order timer, and run FSM)
 	go orderTimer(startOrderTimer, endOrderTimer, orderTimeout)
 	go doorTimer(doorTimeout, doorResetTimer)
-	go runStateMachine()
+	go runStateMachine(arrivedAtFloor, orderCompleted chan int, newOrder, startOrderTimer, endOrderTimer, orderTimeout chan bool, doorTimeout, doorResetTimer, statusUpdate)
 
 
 
@@ -45,20 +45,31 @@ func runStateMachine(arrivedAtFloor, orderCompleted chan int, newOrder, startOrd
 		select {
 			//Handle new orders
 			case <-newOrder:
-				actionNewOrder()
+				actionNewOrder(orderCompleted, doorTimerReset, startOrderTimer)
 
 			//floor reached --> order completed
-			case <-arrivedAtFloor:
-				actionArrivedAtFloor()
+			case f := <-arrivedAtFloor:
+				actionArrivedAtFloor(f, orderCompleted, doorTimerReset, stopOrderTimer)
 
 			//order timeout --> it takes to loong time before the orer is handled. --> stophandleing the order and give it to another elevator
 			case <-orderTimeout:
-				actionOrderTimeout()
+				//actionOrderTimeout() SKRIV INN KODEN I DENNE FUNKSJONEN
+				//MÅ ENDRE PÅ DENNE KODEN!!!!!
+				driver.SetMotorDirection(def.MD_stop)
+				for f := 0; f < def.N_FLOORS; f++ {
+					for b := 0; b < def.N_BUTTONS; b++ {
+						driver.SetButtonLight(f, b, false)
+					}
+				}
+				driver.SetDoorOpenLight(false)
+				fmt.Printf("Order has timed out. Terminate program. ")
+				os.Exit(1)
 
 			//The door has been open too long --> Close it and restart timer?
 			case <-doorTimeout:
-				actionDoorTimeout()
+				actionDoorTimeout(startOrderTimer)
 		}
+		updateStatus <- true
 	}
 
 
@@ -67,8 +78,27 @@ func runStateMachine(arrivedAtFloor, orderCompleted chan int, newOrder, startOrd
 func actionNewOrder(orderCompleted chan int, startOrderTimer chan bool, doorResetTimer){
 	switch state {
 	case def.Idle:
+		direction, prevDirection = Queue.ChooseElevDirection(lastFloor, prevDirection)
+		if Queue.ElevShouldStop(lastFloor, direction) {
+			doorResetTimer  <- true
+			Queue.DelLocalOrdersAtFloorFromQueue(lastFloor)
+			orderCompleted <- lastFloor
+			elev.SetDoorOpenLight(true)
+			state=def.DoorOpen
+		}
+		else {
+			elev.SetMotorDirection(direction)
+			state = def.Moving
+			startOrderTimer <- true
+		}
+
 
 	case def.DoorOpen:
+		if  Queue.ElevShouldStop(lastFloor, direction){
+			startOrderTimer <- true
+			Queue.DelLocalOrdersAtFloorFromQueue(lastFloor)
+			orderCompleted <- lastFloor
+		}
 
 	default:
 		//The elevator is already handeling an order. --> Ignore.
@@ -77,16 +107,45 @@ func actionNewOrder(orderCompleted chan int, startOrderTimer chan bool, doorRese
 
 }
 
-func actionArrivedAtFloor(){
-
+func actionArrivedAtFloor(newFloor int, orderCompleted chan int, doorTimerReset, stopOrderTimer chan bool){
+lastFloor = newFloor
+elev.SetFloorIndicator(lastFloor)
+switch state {
+case def.Moving:
+	if Queue.ElevShouldStop(lastFloor, direction) {
+		orderCompleted <- lastFloor
+		Queue.DelLocalOrdersAtFloorFromQueue(lastFloor)
+		direction=def.MotorD_stop
+		elev.SetMotorDirection(dir)
+		elev.SetDoorOpenLight(true)
+		state = def.DoorOpen
+		doorTimerReset <- true
+	}
+default:
 
 }
 
-func actionDoorTimeout(){
+}
 
+//DENNE FUNKSJONENE MÅ ENDRES!!
+func actionDoorTimeout(startOrderTimer chan bool){
+	switch state {
+	case def.DoorOpen:
+		driver.SetDoorOpenLight(false)
+		dir, prevDir = queue.ChooseDirection(lastFloor, prevDir)
+		driver.SetMotorDirection(dir)
+		if dir == def.MD_stop {
+			state = def.Idle
+		} else {
+			state = def.Moving
+			startOrderTimer <- true
+		}
+
+	default:
+		// Ignore
+	}
 }
 
 func actionOrderTimeout() {
 	
 }
-
